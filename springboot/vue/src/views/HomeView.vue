48845615ec312cc36601dea5c0c416263de9e3c2
linzhuofei <philip.f.lin@gmail.com>
2023-09-27 23:05:30 +0800

    Vue

--- springboot/vue/src/views/HomeView.vue
+++ springboot/vue/src/views/HomeView.vue
@@ -1,10 +1,22 @@
 <template>
-  <div style="height: 100%">
-    <el-container style="height: 100%">
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246); height: 100%">
-        <el-menu :default-openeds="['1', '3']" style="height: 100%">
+    <el-container style="min-height: 100vh">
+      <el-aside :width="sideWidth + 'px'" style="background-color: rgb(238, 241, 246); box-shadow: 2px 0 6px rgb(0 21 41 / 35%)">
+        <el-menu :default-openeds="['1', '3']" style="min-height: 100%; overflow-x: hidden"
+                  background-color="rgb(48, 65, 86)"
+                  text-color="#fff"
+                  active-text-color="#ffd04b"
+                 :collapse-transition="false"
+                 :collapse="isCollapse"
+        >
+          <div style="height: 60px; line-height: 60px; text-align: center">
+            <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; margin-right: 5px">
+            <b style="color: white" v-show="logoTextShow">後台管理系統</b>
+          </div>
           <el-submenu index="1">
-            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <template slot="title">
+              <i class="el-icon-message"></i>
+              <span slot="title">导航一</span>
+            </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
               <el-menu-item index="1-1">选项1</el-menu-item>
@@ -19,7 +31,10 @@
             </el-submenu>
           </el-submenu>
           <el-submenu index="2">
-            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
               <el-menu-item index="2-1">选项1</el-menu-item>
@@ -34,7 +49,10 @@
             </el-submenu>
           </el-submenu>
           <el-submenu index="3">
-            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+            <template slot="title">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航三</span>
+            </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
               <el-menu-item index="3-1">选项1</el-menu-item>
@@ -52,16 +70,19 @@
       </el-aside>
 
       <el-container>
-        <el-header style="text-align: right; font-size: 12px; border-bottom: 1px solid #ccc; line-height: 60px">
-          <el-dropdown>
+        <el-header style="font-size: 12px; border-bottom: 1px solid #ccc; line-height: 60px; display: flex">
+          <div style="flex: 1; font-size: 18px">
+            <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
+          </div>
+          <el-dropdown style="width: 70px; cursor: pointer">
+            <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
             <i class="el-icon-setting" style="margin-right: 15px"></i>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
+              <el-dropdown-item>個人資訊</el-dropdown-item>
+              <el-dropdown-item>退出</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
-          <span>王小虎</span>
+
         </el-header>
 
         <el-main>
@@ -76,7 +97,7 @@
         </el-main>
       </el-container>
     </el-container>
-  </div>
+
 </template>
 
 <script>
